868462998dcfe489997e3e8d829c3409c5dbb99a
Shockn <yurideoliveirac@gmail.com>
2023-07-04 22:43:29 -0300

    Aleatorizando as frases

--- app/src/main/java/com/example/frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.frasesdodia;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void gerarNovaFrase(View view){
+
+        String [] frases = {
+                "F1",
+                "F2",
+                "F3",
+                "F4"
+        };
+
+        int i = new Random().nextInt(4);
+
+        TextView texto = findViewById(R.id.fraseDoDia);
+        texto.setText(frases[i]);
+
+    }
+
 }
